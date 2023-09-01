be11d5f8f22268a6fbd876d56ddbdc33e5473efa
mdalamin706688 <mdalamin706688@gmail.com>
2023-09-02 01:00:21 +0600

    seventh commit

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+     path('', views.login_view, name='login'), 
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),  # Define the login URL pattern
     path('edit_profile/', views.edit_profile, name='edit_profile'),
