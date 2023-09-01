aad074acd9ba3bcfcd5d97c41018ce143323a7e7
mdalamin706688 <mdalamin706688@gmail.com>
2023-09-02 00:40:25 +0600

    five commit

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1r$d3fp3s6_vcy2t1_a6v22-=nj*w)ynab-&%y783r3+8rs49$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False,
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
